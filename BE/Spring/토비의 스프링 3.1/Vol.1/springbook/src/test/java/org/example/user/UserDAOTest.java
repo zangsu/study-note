ad5c98e31e3cff9ac3ef30c9e4198666c4b206e4
zangsu <zangsu_@naver.com>
2023-07-03 21:24:05 +0900

    use Assertj.assertThat() to test automatically

--- BE/Spring/토비의 스프링 3.1/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
+++ BE/Spring/토비의 스프링 3.1/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
@@ -2,12 +2,15 @@ package org.example.user;
 
 import org.example.user.dao.UserDAO;
 import org.example.user.domain.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 public class UserDAOTest {
 
     @Test
@@ -22,38 +25,9 @@ public class UserDAOTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
 
-
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
     }
-
-   /* public static  void newDaoEqualsTest(){
-
-        DaoFactory factory = new DaoFactory();
-        UserDAO dao1 = factory.userDAO();
-        UserDAO dao2 = factory.userDAO();
-
-        System.out.println(dao1);
-        System.out.println(dao2);
-
-    }
-
-    public static void getDaoEqualsTest(){
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
-        UserDAO dao1 = context.getBean("userDAO", UserDAO.class);
-        UserDAO dao2 = context.getBean("userDAO", UserDAO.class);
-
-        System.out.println(dao1);
-        System.out.println(dao2);
-    }*/
 }
