d0359c34e8a6f5ab0fe34fbd63ae7eefbc81da04
zangsu <zangsu_@naver.com>
2023-07-04 21:43:15 +0900

    [CH3] refactor : apply strategy patern in deleteAll() by extract staic code to method

--- BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
@@ -67,31 +67,8 @@ public class UserDAO {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c= null;
-        PreparedStatement ps = null;
-
-        try{
-            c = dataSource.getConnection();
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        }catch (SQLException e){
-            throw e;
-        }finally {
-            if(ps != null){
-                try{
-                    ps.close();
-                }catch (SQLException e){
-                }
-            }
-            if(c != null){
-                try{
-                    c.close();
-                }catch (SQLException e){
-                }
-            }
-        }
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -128,6 +105,31 @@ public class UserDAO {
                     c.close();
                 }catch (SQLException e){
                 }
+            }
+        }
+
+    }
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try{
+            c = dataSource.getConnection();
+
+            ps = stmt.makePreparedStatement(c);
+        } catch (SQLException e) {
+            throw e;
+        }finally {
+            if(ps != null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+            if(c != null){
+                try{
+                    c.close();
+                } catch (SQLException e) {}
             }
         }
     }
