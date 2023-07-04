3264d5f6862cf9bc3ca8ecf50bbb8a45cc19b74d
unknown <zangsu_@naver.com>
2023-07-04 14:00:54 +0900

    [CH2] refactor : make ApplicationContext inserted by SpringContext

--- BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
@@ -6,15 +6,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDAOTest {
+    @Autowired
+    private ApplicationContext context;
+
     private UserDAO dao;
 
     private User user1;
@@ -23,7 +34,11 @@ public class UserDAOTest {
 
     @BeforeEach
     public void setUp(){
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        //테스트 클래스가 매 순간 생성되며, 이 때 항상 컨텍스트는 공유한다.
+        System.out.println(this.context); //같은 객체임을 확인
+        System.out.println(this); //다른 객체임을 확인
+
         this.dao = context.getBean("userDAO", UserDAO.class);
 
         this.user1 = new User("toby", "이일민", "toby");
