9b87676216f8e4ed3b1f366d675b2129f4b84599
zangsu <zangsu_@naver.com>
2023-07-04 21:51:08 +0900

    [CH3] refactor : make AddStatement class to anonymous inner class

--- BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
@@ -23,9 +23,19 @@ public class UserDAO {
     }
 
 
-    public void add(User user) throws SQLException {
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextWithStatementStrategy(st);
+    public void add(final User user) throws SQLException {
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(
+                        "insert into users(id, name, password) values (?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        });
     }
 
     public User get(String id) throws SQLException {
