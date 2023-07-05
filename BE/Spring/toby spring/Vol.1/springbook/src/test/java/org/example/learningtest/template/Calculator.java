1c46492a4e4bb208704e083cd1c52c409fdb490c
zangsu <zangsu_@naver.com>
2023-07-05 18:12:46 +0900

    [CH3] refactor : use try-catch-finally in calcSum()

--- BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/learningtest/template/Calculator.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/learningtest/template/Calculator.java
@@ -4,14 +4,26 @@ import java.io.*;
 
 public class Calculator {
     public Integer calcSum(String filePath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        Integer sum = 0;
-        String line = null;
-        while((line = br.readLine()) != null){
-            sum += Integer.valueOf(line);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            String line = null;
+            Integer sum = 0;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }finally {
+            if(br != null){
+                try{
+                    br.close();
+                }catch(IOException e){
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-        br.close();
-        return sum;
     }
 }
