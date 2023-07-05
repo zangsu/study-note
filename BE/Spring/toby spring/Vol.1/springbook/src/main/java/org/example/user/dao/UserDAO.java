0057a93517b6b77f516942c7fdd24189faadee91
zangsu <zangsu_@naver.com>
2023-07-05 12:50:05 +0900

    [CH2] modify test

--- BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
@@ -2,6 +2,7 @@ package org.example.user.dao;
 
 import lombok.NoArgsConstructor;
 import org.example.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -46,16 +47,21 @@ public class UserDAO {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        this.user = new User();
-        this.user.setId(rs.getString(   "id"));
-        this.user.setName(rs.getString("name"));
-        this.user.setPassword(rs.getString("password"));
+
+        User user = null;
+        if(rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
 
+        if(user == null)
+            throw new EmptyResultDataAccessException(1);
         return user;
     }
 
--- BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/UserDAOTest.java
@@ -2,6 +2,7 @@ package org.example.user;
 
 import org.example.user.dao.UserDAO;
 import org.example.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -12,26 +13,44 @@ import java.sql.SQLException;
 import static org.assertj.core.api.Assertions.assertThat;
 public class UserDAOTest {
 
+    private UserDAO dao;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    public void setUp(){
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDAO dao = context.getBean("userDAO", UserDAO.class);
+        this.user1 = new User("toby", "이일민", "toby");
+        this.user2 = new User("holyeye", "김영한", "holyeye");
+        this.user3 = new User("zangsu", "장혁수", "zangsu");
+
+    }
+
     @Test
     public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDAO dao = context.getBean("userDAO", UserDAO.class);
 
+        User user1 = new User("toby", "이일민", "toby");
+        User user2 = new User("holyeye", "김영한", "holyeye");
+
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("toby");
-        user.setName("이일민");
-        user.setPassword("toby");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
 
-        dao.add(user);
-        assertThat(dao.getCount()).isEqualTo(1);
+        User userGet1 = dao.get(user1.getId());
+        assertThat(userGet1.getName()).isEqualTo(user1.getName());
+        assertThat(userGet1.getPassword()).isEqualTo(user1.getPassword());
 
-        User user2 = dao.get(user.getId());
-
-        assertThat(user2.getName()).isEqualTo(user.getName());
-        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+        User userGet2 = dao.get(user2.getId());
+        assertThat(userGet2.getName()).isEqualTo(user2.getName());
+        assertThat(userGet2.getPassword()).isEqualTo(user2.getPassword());
     }
 
     @Test
@@ -39,9 +58,6 @@ public class UserDAOTest {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDAO dao = context.getBean("userDAO", UserDAO.class);
 
-        User user1 = new User("toby", "이일민", "toby");
-        User user2 = new User("holyeye", "김영한", "holyeye");
-        User user3 = new User("zangsu", "장혁수", "zangsu");
 
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
@@ -55,4 +71,5 @@ public class UserDAOTest {
         dao.add(user3);
         assertThat(dao.getCount()).isEqualTo(3);
     }
+
 }
