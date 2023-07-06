98f2d465ca6bc4958134b8398c277eca8995a00f
zangsu <zangsu_@naver.com>
2023-07-06 22:12:45 +0900

    [CH3] refactor : modify UserDAO.getCount() to use jdbcTemplate

--- BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/dao/UserDAO.java
@@ -2,9 +2,11 @@ package org.example.user.dao;
 
 import lombok.NoArgsConstructor;
 import org.example.user.domain.User;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -67,42 +69,6 @@ public class UserDAO {
     }
 
     public int getCount() throws SQLException {
-
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try{
-            c = dataSource.getConnection();
-
-            ps = c.prepareStatement("select count(*) from users");
-
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        }catch (SQLException e){
-            throw e;
-        }finally {
-            if(rs != null){
-                try{
-                    rs.close();
-                }catch (SQLException e){
-                }
-            }
-            if(ps != null){
-                try{
-                    ps.close();
-                }catch (SQLException e){
-                }
-            }
-            if(c != null){
-                try{
-                    c.close();
-                }catch (SQLException e){
-                }
-            }
-        }
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
-
-
 }
