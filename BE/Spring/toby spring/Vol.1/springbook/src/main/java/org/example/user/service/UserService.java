21289de810e80b44009e3704c3f0d33c90288b74
zangsu <zangsu_@naver.com>
2023-07-23 14:53:57 +0900

    [CH5] feat : add manage level logic in UserService

--- BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/service/UserService.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/main/java/org/example/user/service/UserService.java
@@ -1,11 +1,38 @@
 package org.example.user.service;
 
 import org.example.user.dao.UserDAO;
+import org.example.user.domain.Level;
+import org.example.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
     UserDAO userDAO;
 
+    static final int REQ_LOGIN = 50;
+    static final int REQ_RECOMMEND = 30;
+
     public void setUserDAO(UserDAO userDAO){
         this.userDAO = userDAO;
+    }
+
+    public void upgradeLevels(){
+        List<User> users = userDAO.getAll();
+
+        for (User user : users) {
+
+            Boolean changed = null;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= REQ_LOGIN) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= REQ_RECOMMEND) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            }else
+                changed = false;
+
+            if(changed)
+                userDAO.update(user);
+        }
     }
 }
--- BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/service/UserServiceTest.java
+++ BE/Spring/toby spring/Vol.1/springbook/src/test/java/org/example/user/service/UserServiceTest.java
@@ -1,12 +1,18 @@
 package org.example.user.service;
 
 import org.assertj.core.api.Assertions;
+import org.example.user.dao.UserDAO;
+import org.example.user.domain.Level;
+import org.example.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
 
 
 @ExtendWith(SpringExtension.class)
@@ -15,6 +21,20 @@ class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserDAO userDAO;
+
+    @BeforeEach
+    public void setUp(){
+        userDAO.deleteAll();
+
+        userDAO.add(new User("1toby", "이일민", "toby", Level.BASIC, 49, 0));
+        userDAO.add(new User("2swg0605", "손원준", "swg_pw", Level.BASIC, 50, 0));
+        userDAO.add(new User("3holyeye", "김영한", "holyeye", Level.SILVER, 55, 29));
+        userDAO.add(new User("4moonGD", "문경덕", "moon_pw", Level.SILVER, 60, 39));
+        userDAO.add(new User("5zangsu", "장혁수", "zangsu", Level.GOLD, 100, 40));
+    }
+
     @Test
     public void bean() throws Exception{
         //given
@@ -22,5 +42,27 @@ class UserServiceTest {
         //when
 
         //then
+    }
+
+    @Test
+    public void upgradeLevels() throws Exception{
+        //given
+        List<User> users = userDAO.getAll();
+
+        //when
+        userService.upgradeLevels();
+
+        //then
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+
+    }
+
+    private void checkLevel(User user, Level expectedLevel){
+        User userUpdate = userService.userDAO.get(user.getId());
+        Assertions.assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
     }
 }
